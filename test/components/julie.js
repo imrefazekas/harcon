210bacb53635d06416c317034a3973744ae7b9f1
Imre Fazekas <imre.fazekas@icloud.com>
2015-02-14 10:05:23 +0100

    refinements

--- test/components/julie.js
+++ test/components/julie.js
@@ -3,6 +3,7 @@ module.exports = {
 	context: 'morning',
 	// When Julie is woken up, send a gentle message to everyone listening to such messages...  Walter and Pater namely
 	wakeup: function( ignite, callback ){
+		this.harconlog( null, 'Simple logging test', {}, 'warn' );
 		ignite( 'greet.gentle', 'It is morning!', 'Time to wake up!', function(err, res){
 			callback(err, res);
 		} );
