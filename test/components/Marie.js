62b84f342fb3a479c2bfde722d11dc6f43ed6d98
Imre Fazekas <imre.fazekas@icloud.com>
2015-06-16 13:27:09 +0200

    shifted function added

--- test/components/Marie.js
+++ test/components/Marie.js
@@ -7,6 +7,7 @@ module.exports = {
 	// Simple service function listening to the greet.simple message where greet comes from context and simple is identified by the name of the fuction.
 	simple: function (greetings1, greetings2, callback) {
 		this.greetings = [greetings1, greetings2];
+		this.shifted( { data: 'content' } );
 		callback(null, 'Bonjour!');
 	}
 };
