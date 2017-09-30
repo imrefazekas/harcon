7c14ff056f3b3f73752ef2c7d897571747d6fb8a
Imre Fazekas <imre.fazekas@icloud.com>
2017-09-30 22:59:16 +0200

    Mortar can be initiated multiple times

--- util/Mortar.js
+++ util/Mortar.js
@@ -14,121 +14,126 @@ let RESERVATION = [ 'Barrel', 'Bender', 'Blower', 'Communication', 'Fire', 'Fire
 let Assigner = require('assign.js')
 let assigner = new Assigner()
 
-module.exports = {
-	name: 'Mortar',
-	files: [],
-	init: async function (options = {}) {
-		let self = this
-		self.options = options
-
-		if (!self.configs)
-			self.configs = {}
-		if (!self.globalConfig)
-			self.globalConfig = {}
-		self.watchMonitors = []
-
-		let extension = '.js'
-		self.matcher = self.options.matcher || ( self.options.pattern ? function (filePath) { return self.options.pattern.test(filePath) } : function (filePath) { return filePath.endsWith( extension ) } )
-
-		if ( !fs.existsSync( self.options.folder ) )
-			mkdirp.sync( self.options.folder )
-
-		self.files = []
-
-		await self.firstRead()
-		return 'ok'
-	},
-	firstRead: async function () {
-		let self = this
-
-		if ( self.options.waitFor && self.options.waitFor.entity && !self.inflicterContext._barrel.firestarter( self.options.waitFor.entity ) ) {
-			return setTimeout( () => {
-				self.firstRead().then( () => {} ).catch( (reason) => { self.harconlog(reason) } )
-			}, self.options.waitFor.timeout || 1000 )
-		}
-
-		let isComponent = function (filePath, stat) {
-			return !stat.isDirectory() && self.matcher(filePath)
-		}
-
-		await self.readFiles( self.options.folder )
-
-		await self.igniteFiles( )
-
-		if ( self.options.liveReload ) {
-			watch.createMonitor( self.options.folder, function (monitor) {
-				self.watchMonitors.push( monitor )
-				let handler = function (f, stat) {
-					if ( isComponent( f, stat ) )
-						self.scheduleFile( null, f )
-				}
-				events.forEach(function (eventName) {
-					monitor.on( eventName, handler )
-				})
-			})
-			self.setInterval( async function () {
-				self.harconlog( null, 'Mortar is checking for entity changes', null, 'trace' )
-				await self.igniteFiles( )
-			}, self.options.liveReloadTimeout || 5000 )
-		}
-		return 'ok'
-	},
-	addGlobalConfig: function ( config ) {
-		this.globalConfig = config
-
-		return this
-	},
-	addConfig: function ( name, config ) {
-		this.configs[name] = config
-
-		return this
-	},
-	scheduleFile: function ( folder, fileName ) {
-		let fPath = folder ? folder + path.sep + fileName : fileName
-		if ( this.files.indexOf( fPath ) === -1 )
-			this.files.push( fPath )
-		return this
-	},
-	igniteFiles: async function ( ) {
-		let self = this
-
-		let newFiles = self.files.slice()
-		self.files.length = 0
-		// liveReloadTimeout: -1
-		newFiles.forEach( async function (newFile) {
-			if ( fs.existsSync( newFile ) ) {
-				self.harconlog( null, '(Re)New entity file', newFile, 'info' )
-				let compName = newFile.substring( 0, newFile.length - 3 )
-				delete require.cache[require.resolve( compName )]
-				let component = require( compName )
-				if ( self.options.entityPatcher )
-					self.options.entityPatcher( component )
-				if ( !component.name )
-					throw new Error( 'Entity has no name', newFile )
-				if ( RESERVATION.find( (name) => { return name.toLowerCase() === component.name.toLowerCase() } ) )
-					throw new Error( 'Entity has forbidden name', newFile )
-				if ( component.adequate && !component.adequate() )
-					throw new Error( 'Entity failed to be adequate', newFile )
-				await self.ignite( 'Inflicter.addicts', component, assigner.assign( {}, self.globalConfig[component.name], self.configs[component.name] ) )
-			} else {
-				self.harconlog( null, 'Removed entity file', newFile, 'info' )
-				await self.ignite( 'Inflicter.detracts', path.basename( newFile, '.js') )
-			}
-		} )
-		return 'ok'
-	},
-	readFiles: async function ( folder ) {
-		let files = await readdir( folder )
-		for (let i = 0; i < files.length; i += 1)
-			if ( this.matcher(files[i]) )
-				this.scheduleFile( folder, files[i] )
-		return 'ok'
-	},
-	close: async function ( ) {
-		this.watchMonitors.forEach( function ( monitor ) {
-			monitor.stop()
-		} )
-		this.watchMonitors.length = 0
-		return 'Stopped'
-	}
+function Mortar () {
+	this.name = 'Mortar'
+	this.files = []
 }
+
+let mortar = Mortar.prototype
+
+mortar.init = async function (options = {}) {
+	let self = this
+	self.options = options
+
+	if (!self.configs)
+		self.configs = {}
+	if (!self.globalConfig)
+		self.globalConfig = {}
+	self.watchMonitors = []
+
+	let extension = '.js'
+	self.matcher = self.options.matcher || ( self.options.pattern ? function (filePath) { return self.options.pattern.test(filePath) } : function (filePath) { return filePath.endsWith( extension ) } )
+
+	if ( !fs.existsSync( self.options.folder ) )
+		mkdirp.sync( self.options.folder )
+
+	self.files = []
+
+	await self.firstRead()
+	return 'ok'
+}
+mortar.firstRead = async function () {
+	let self = this
+
+	if ( self.options.waitFor && self.options.waitFor.entity && !self.inflicterContext._barrel.firestarter( self.options.waitFor.entity ) ) {
+		return setTimeout( () => {
+			self.firstRead().then( () => {} ).catch( (reason) => { self.harconlog(reason) } )
+		}, self.options.waitFor.timeout || 1000 )
+	}
+
+	let isComponent = function (filePath, stat) {
+		return !stat.isDirectory() && self.matcher(filePath)
+	}
+
+	await self.readFiles( self.options.folder )
+
+	await self.igniteFiles( )
+
+	if ( self.options.liveReload ) {
+		watch.createMonitor( self.options.folder, function (monitor) {
+			self.watchMonitors.push( monitor )
+			let handler = function (f, stat) {
+				if ( isComponent( f, stat ) )
+					self.scheduleFile( null, f )
+			}
+			events.forEach(function (eventName) {
+				monitor.on( eventName, handler )
+			})
+		})
+		self.setInterval( async function () {
+			self.harconlog( null, 'Mortar is checking for entity changes', null, 'trace' )
+			await self.igniteFiles( )
+		}, self.options.liveReloadTimeout || 5000 )
+	}
+	return 'ok'
+}
+mortar.addGlobalConfig = function ( config ) {
+	this.globalConfig = config
+
+	return this
+}
+mortar.addConfig = function ( name, config ) {
+	this.configs[name] = config
+
+	return this
+}
+mortar.scheduleFile = function ( folder, fileName ) {
+	let fPath = folder ? folder + path.sep + fileName : fileName
+	if ( this.files.indexOf( fPath ) === -1 )
+		this.files.push( fPath )
+	return this
+}
+mortar.igniteFiles = async function ( ) {
+	let self = this
+
+	let newFiles = self.files.slice()
+	self.files.length = 0
+	// liveReloadTimeout: -1
+	newFiles.forEach( async function (newFile) {
+		if ( fs.existsSync( newFile ) ) {
+			self.harconlog( null, '(Re)New entity file', newFile, 'info' )
+			let compName = newFile.substring( 0, newFile.length - 3 )
+			delete require.cache[require.resolve( compName )]
+			let component = require( compName )
+			if ( self.options.entityPatcher )
+				self.options.entityPatcher( component )
+			if ( !component.name )
+				throw new Error( 'Entity has no name', newFile )
+			if ( RESERVATION.find( (name) => { return name.toLowerCase() === component.name.toLowerCase() } ) )
+				throw new Error( 'Entity has forbidden name', newFile )
+			if ( component.adequate && !component.adequate() )
+				throw new Error( 'Entity failed to be adequate', newFile )
+			await self.ignite( 'Inflicter.addicts', component, assigner.assign( {}, self.globalConfig[component.name], self.configs[component.name] ) )
+		} else {
+			self.harconlog( null, 'Removed entity file', newFile, 'info' )
+			await self.ignite( 'Inflicter.detracts', path.basename( newFile, '.js') )
+		}
+	} )
+	return 'ok'
+}
+mortar.readFiles = async function ( folder ) {
+	let files = await readdir( folder )
+	for (let i = 0; i < files.length; i += 1)
+		if ( this.matcher(files[i]) )
+			this.scheduleFile( folder, files[i] )
+	return 'ok'
+}
+mortar.close = async function ( ) {
+	this.watchMonitors.forEach( function ( monitor ) {
+		monitor.stop()
+	} )
+	this.watchMonitors.length = 0
+	return 'Stopped'
+}
+
+module.exports = Mortar
