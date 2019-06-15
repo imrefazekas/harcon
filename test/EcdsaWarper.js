1def9f221b915a221b5d76f376db9f807a31d94a
Imre Fazekas <imre.fazekas@icloud.com>
2019-06-15 19:01:30 +0200

    warper fix

--- test/EcdsaWarper.js
+++ test/EcdsaWarper.js
@@ -1,22 +1,26 @@
 const { randomBytes } = require('crypto')
-const secp256k1 = require('secp256k1')
+const { secp256k1 } = require('@nlv8/signun')
 
 function Warper ( division, connectedDivisions, options ) {
 	this.division = division
 	this.connectedDivisions = connectedDivisions || []
 
-	let privKey
-	do {
-		privKey = randomBytes(32)
-	} while (!secp256k1.privateKeyVerify(privKey))
-	this.privKey = privKey
-	this.pubKey = secp256k1.publicKeyCreate(privKey)
-
 	this.message = Buffer.from( options.message, 'hex' )
-	this.sigObj = secp256k1.sign( this.message, this.privKey )
 }
 
 let warper = Warper.prototype
+
+warper.init = async function () {
+	let privKey
+	do {
+		privKey = randomBytes(32)
+	} while ( !(await secp256k1.privateKeyVerify(privKey)) )
+
+	this.privKey = privKey
+	this.pubKey = await secp256k1.publicKeyCreate(privKey)
+
+	this.sigObj = await secp256k1.sign( this.message, this.privKey )
+}
 
 warper.conform = function ( comm ) {
 	try {
@@ -27,12 +31,8 @@ warper.conform = function ( comm ) {
 warper.referenceMatrix = function ( object ) {
 }
 
-warper.allow = function ( comm ) {
-	try {
-		return secp256k1.verify( this.message, Buffer.from(comm.signature, 'hex'), this.pubKey )
-	} catch (err) {
-		return false
-	}
+warper.allow = async function ( comm ) {
+	return secp256k1.verify( this.message, Buffer.from(comm.signature, 'hex'), this.pubKey )
 }
 
 module.exports = Warper
